23f73e89090eae26d68f9cb0ad0a6c0358469c12
Thiago Macedo <thialmaceod@gmail.com>
2023-11-01 14:12:22 -0300

    error control"

--- src/network/machine.py
+++ src/network/machine.py
@@ -1,16 +1,23 @@
+import time
+import random
 import socket
+import threading
 from .packet import Packet
 from .data_packet import DataPacket
 from .token_packet import TokenPacket
 
 class Machine:
     
-    def __init__(self, ip: str, nickname: str, time_token: str, has_token: bool = False) -> None:
+    def __init__(self, ip: str, nickname: str, time_token: str, has_token: bool = False, error_probability: float = 0.2, TIMEOUT_VALUE: int = 10, MINIMUM_TIME: int = 10) -> None:
         self.ip = Machine.get_ip(ip)
         self.port = int(Machine.get_port(ip))
         self.nickname = nickname
         self.time_token = time_token
+        self.controls_token = False
         self.has_token = has_token
+        self.error_probability = error_probability
+        self.TIMEOUT_VALUE = TIMEOUT_VALUE 
+        self.MINIMUM_TIME = MINIMUM_TIME
         
         if self.has_token == True:
             self.controls_token = True
@@ -20,6 +27,11 @@ class Machine:
         
         if self.has_token == True:
             self.generate_token()
+            
+        self.last_token_time = None
+        if self.controls_token:
+            self.token_checker_thread = threading.Thread(target=self.check_token_status)
+            self.token_checker_thread.start()
         
     def generate_token(self):
         self.token = TokenPacket()
@@ -28,7 +40,11 @@ class Machine:
         self.message_queue.append(packet)
         
     def send_packet(self, packet: Packet):
-        # inserir erro no pacote com probabilidade
+        if isinstance(packet, DataPacket):
+            if random.random() < self.error_probability:
+                packet.crc = packet.crc[:-1] + ('0' if packet.crc[-1] == '1' else '1')
+                print(f"Erro introduzido no pacote com destino: {packet.destination_name}")
+                
         self.socket.sendto(packet.header.encode(), (self.ip, self.port))
         
     def receive_packet(self):
@@ -67,15 +83,21 @@ class Machine:
         if self.has_token == True:
             if len(self.message_queue) > 0:
                 packet = self.message_queue[0]
+                print("Mensagem enviada para: " + packet.destination_name)
                 self.send_packet(packet)
             else:
+                print("Nenhuma mensagem para enviar, passando token...")
                 self.send_packet(self.token)
                 self.has_token = False
         
     def process_packet(self, packet: Packet):
         if packet.id == "1000":
-            self.has_token = True
-            self.run() # roda o algoritmo para ver se tem que mandar alguma coisa
+            self.last_token_time = time.time()
+            if not self.has_token:
+                self.has_token = True
+                self.run()
+            else:
+                pass
             
         elif packet.id == "2000":
             
@@ -95,6 +117,7 @@ class Machine:
                     self.message_queue.pop(0) # tira da fila
                 elif packet.error_control == "NACK":
                     print("Erro na mensagem: " + packet.message) # imprime log
+                    self.send_packet(packet) # reenvia o pacote se houver erro
                 elif packet.error_control == "maquinanaoexiste":
                     print("Máquina não existe: " + packet.message) # imprime log
                     self.message_queue.pop(0) # tira da fila
@@ -113,3 +136,20 @@ class Machine:
             else:
                 # passa para o próximo
                 self.send_packet(packet)
+
+    def check_token_status(self):
+        while True:
+            time.sleep(1)  # Verifica o status do token a cada segundo (ajuste conforme necessário)
+            
+            if self.last_token_time is None:
+                continue
+            
+            time_since_last_token = time.time() - self.last_token_time
+            
+            if time_since_last_token > self.TIMEOUT_VALUE:  # TIMEOUT_VALUE é o tempo máximo permitido sem ver o token
+                print("Token não visto por muito tempo. Gerando novo token.")
+                self.generate_token()
+            
+            elif time_since_last_token < self.MINIMUM_TIME:  # MINIMUM_TIME é o tempo mínimo esperado entre as passagens do token
+                print("Token visto muito rapidamente. Retirando token da rede.")
+                self.has_token = False
